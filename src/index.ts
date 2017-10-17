8b262f9dd398e3281678c2bb793e0a069a6339de
gthomas <gthomas@geologging.com>
2017-10-17 16:30:00 +0100

    expose unit conversion functions

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./deg-to-grad";
+export * from "./deg-to-rad";
+export * from "./grad-to-deg";
+export * from "./grad-to-rad";
+export * from "./rad-to-deg";
+export * from "./rad-to-grad";
